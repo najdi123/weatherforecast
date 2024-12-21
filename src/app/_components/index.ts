4a352a41d1a6caf751ea311f224663e80bb7baa6
monire91 <aglida1370@gmail.com>
2024-12-21 14:01:58 -0600

    barrel file for clean import

---
+++ src/app/_components/index.ts
@@ -0,0 +1,4 @@
+export { default as LocationPermission } from './LocationPermission';
+export { default as ForecastItem } from './Forecast/ForecastItem';
+export { default as ForecastList } from './Forecast/ForecastList';
+export { dict } from './dict';
